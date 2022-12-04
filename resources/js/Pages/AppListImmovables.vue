158a89c92114feeb00c0fcfd4a702d0092e39d21
IvanVaskovich <82166299+IvanVaskovich@users.noreply.github.com>
2022-12-04 20:35:59 +0300

    Front

---
+++ resources/js/Pages/AppListImmovables.vue
@@ -0,0 +1,36 @@
+<template>
+  <app-header
+    @login-developer="openLoginDeveloper = !openLoginDeveloper"
+    @login-realtor="openLoginRealtor = !openLoginRealtor"
+  />
+  <main>
+    <div class="_container">
+      <div class="immovables__grid">
+        <div class="bg-[] p-7 ">
+
+        </div>
+
+        <div></div>
+
+        <div></div>
+      </div>
+    </div>
+  </main>
+  <app-footer />
+</template>
+
+<script>
+import AppHeader from '../Layouts/AppHeader.vue'
+import AppFooter from "../Layouts/AppFooter.vue"
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
