d03334c5cc2d78881e41d7caae4c31f5d70cf123
IvanVaskovich <82166299+IvanVaskovich@users.noreply.github.com>
2022-12-17 16:09:42 +0300

    AppNews

---
+++ resources/js/Layouts/AppModalProfile.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="fixed z-[100] w-full h-full flex items-center justify-center">
+    <div class="relative flex flex-col gap-7 xxl:gap-6 xl:gap-5 bg-white rounded-[12px] px-12 xxl:px-10 xl:px-8 py-8 xxl:py-6 xl:py-5 z-50 w-[30%] h-fit">
+      <div class="relative flex justify-between items-center">
+        <h2 class=" text-[22px] xxl:text-lg xl:text-base font-semibold leading-none">Оплата подписки</h2>
+
+      <button class="bg-[#E84680] rounded-[5px] w-full py-5 xxl:py-4 xl:py-3">
+        <span class="text-white font-semibold text-lg xxl:text-[15px] xl:text-[13px] leading-none">Оплатить</span>
+      </button>
+    </div>
+    <div @click="this.$emit('close-add-frame')" class="absolute bg-black opacity-50 h-full w-full z-40"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/AppPrivateOfficeDev.vue
+++ resources/js/Pages/AppPrivateOfficeDev.vue
@@ -13,15 +13,15 @@ import { Link } from '@inertiajs/inertia-vue3'
         </div>
         <div class="grid grid-cols-2 gap-7 xxl:gap-5 xl:gap-4">
 
-          <div class="flex flex-col">
+          <div v-for="object in objects" class="flex flex-col">
             <div class="object__block relative h-[26vw]">
               <img src="../../assets/immovables_img_one.png" class="w-full h-full rounded-[8px]" alt="">
-              <div class="seek hidden absolute top-0 left-0 immovables__overlay w-full h-full rounded-[8px]"></div>
-              <div class="hide absolute flex gap-2 top-0 left-0 p-5 xxl:p-4 xl:p-3">
+              <div class="seek opacity-0 transition-all absolute top-0 left-0 immovables__overlay w-full h-full rounded-[8px]"></div>
+              <div class="hide transition-all absolute flex gap-2 top-0 left-0 p-5 xxl:p-4 xl:p-3">
                 <span class="bg-[#30CB49] rounded-[3px] px-2 xxl:px-1.5 xl:px-1 py-1.5 xxl:py-1 xl:py-0.5 text-white text-sm xxl:text-xs xl:text-[10px] leading-none">опубликованно</span>
                 <span class="bg-[#E84680] rounded-[3px] px-2 xxl:px-1.5 xl:px-1 py-1.5 xxl:py-1 xl:py-0.5 text-white text-sm xxl:text-xs xl:text-[10px] leading-none">не прошло модерацию</span>
               </div>
-              <div class="seek hidden absolute z-10 top-1/2 w-full flex flex-col gap-3 xxl:gap-2 xl:gap-1.5 items-center -translate-x-1/2 -translate-y-1/2 left-1/2">
+              <div class="seek opacity-0 transition-all absolute z-10 top-1/2 w-full flex flex-col gap-3 xxl:gap-2 xl:gap-1.5 items-center -translate-x-1/2 -translate-y-1/2 left-1/2">
                 <button class="flex items-center justify-between w-[30%] border border-solid border-[#EFEEF580] rounded-[3px] px-3 xxl:px-2 xl:px-1.5 py-3 xxl:py-2 xl:py-1.5">
                   <span class="text-white text-sm xxl:text-xs xl:text-[10px] leading-none">Редактировать</span>
                   <img src="../../assets/svg/pen_icon_white.svg" class="w-4.5 xxl:w-3.5 xl:w-3" alt="">
@@ -47,67 +47,15 @@ import { Link } from '@inertiajs/inertia-vue3'
             <div class="flex flex-col p-5 xxl:p-4 xl:p-3">
               <span class="font-semibold text-xl xxl:text-base xl:text-[13px]">Euro Avsallar Residence</span>
               <div class="relative">
-                <span @click="openWatchTime = !openWatchTime" class="w-fit cursor-pointer flex items-center gap-2 xxl:gap-1.5 xl:gap-1">
+                <span @click="object.active = !object.active" class="w-fit cursor-pointer flex items-center gap-2 xxl:gap-1.5 xl:gap-1">
                   <img src="../../assets/svg/eye_icon_grey.svg" class="w-4 xxl:w-3.5 xl:w-3" alt="">
                   <span class="text-[#8A8996] leading-none text-lg xxl:text-[15px] xl:text-[13px]"><span class="text-[#6435A5]"> 220 </span> {{ selectTime }}</span>
                   <img src="../../assets/svg/arrow_down_black.svg" alt="">
                 </span>
-                <div v-if="openWatchTime" class="absolute top-[120%] text-lg xxl:text-[15px] xl:text-[13px] rounded-[3px] z-40 border bg-white border-solid border-[#E5DFEE] flex flex-col">
+                <div v-if="object.active" class="absolute top-[120%] text-lg xxl:text-[15px] xl:text-[13px] rounded-[3px] z-40 border bg-white border-solid border-[#E5DFEE] flex flex-col">
                   <span
                     v-for="(time, idx) in watchTime" :key="idx"
-                    @click="changeSelectTime(time)"
-                    class="hover__select cursor-pointer pl-5 xxl:pl-4 xl:pl-3 pr-20 xxl:pr-16 xl:pr-12 py-3 xxl:py-2.5 xl:py-2 leading-none"
-                  >
-                    {{ time.time }}
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="flex flex-col">
-            <div class="object__block relative h-[26vw]">
-              <img src="../../assets/immovables_img_one.png" class="w-full h-full rounded-[8px]" alt="">
-              <div class="seek hidden absolute top-0 left-0 immovables__overlay w-full h-full rounded-[8px]"></div>
-              <div class="hide absolute flex gap-2 top-0 left-0 p-5 xxl:p-4 xl:p-3">
-                <span class="bg-[#30CB49] rounded-[3px] px-2 xxl:px-1.5 xl:px-1 py-1.5 xxl:py-1 xl:py-0.5 text-white text-sm xxl:text-xs xl:text-[10px] leading-none">опубликованно</span>
-                <span class="bg-[#E84680] rounded-[3px] px-2 xxl:px-1.5 xl:px-1 py-1.5 xxl:py-1 xl:py-0.5 text-white text-sm xxl:text-xs xl:text-[10px] leading-none">не прошло модерацию</span>
-              </div>
-              <div class="seek hidden absolute z-10 top-1/2 w-full flex flex-col gap-3 xxl:gap-2 xl:gap-1.5 items-center -translate-x-1/2 -translate-y-1/2 left-1/2">
-                <button class="flex items-center justify-between w-[30%] border border-solid border-[#EFEEF580] rounded-[3px] px-3 xxl:px-2 xl:px-1.5 py-3 xxl:py-2 xl:py-1.5">
-                  <span class="text-white text-sm xxl:text-xs xl:text-[10px] leading-none">Редактировать</span>
-                  <img src="../../assets/svg/pen_icon_white.svg" class="w-4.5 xxl:w-3.5 xl:w-3" alt="">
-                </button>
-                <button class="flex items-center justify-between w-[30%] border border-solid border-[#EFEEF580] rounded-[3px] px-3 xxl:px-2 xl:px-1.5 py-3 xxl:py-2 xl:py-1.5">
-                  <span class="text-white text-sm xxl:text-xs xl:text-[10px] leading-none">Удалить</span>
-                  <img src="../../assets/svg/bucket_icon_white.svg" class="w-4.5 xxl:w-3.5 xl:w-3" alt="">
-                </button>
-                <button class="flex items-center justify-between w-[30%] border border-solid border-[#EFEEF580] rounded-[3px] px-3 xxl:px-2 xl:px-1.5 py-3 xxl:py-2 xl:py-1.5">
-                  <span class="text-white text-sm xxl:text-xs xl:text-[10px] leading-none">Скрыть</span>
-                  <img src="../../assets/svg/hide_icon_white.svg" class="w-4.5 xxl:w-3.5 xl:w-3" alt="">
-                </button>
-                <button class="flex items-center justify-between w-[30%] border border-solid border-[#EFEEF580] rounded-[3px] px-3 xxl:px-2 xl:px-1.5 py-3 xxl:py-2 xl:py-1.5">
-                  <span class="text-white text-sm xxl:text-xs xl:text-[10px] leading-none">Посмотреть на сайте</span>
-                  <img src="../../assets/svg/planet_icon_white.svg" class="w-4.5 xxl:w-3.5 xl:w-3" alt="">
-                </button>
-                <button class="flex items-center justify-between w-[30%] border border-solid border-[#EFEEF580] rounded-[3px] px-3 xxl:px-2 xl:px-1.5 py-3 xxl:py-2 xl:py-1.5">
-                  <span class="text-white text-sm xxl:text-xs xl:text-[10px] leading-none">Добавить новость</span>
-                  <img src="../../assets/svg/plus_icon.svg" class="w-4.5 xxl:w-3.5 xl:w-3" alt="">
-                </button>
-              </div>
-            </div>
-            <div class="flex flex-col p-5 xxl:p-4 xl:p-3">
-              <span class="font-semibold text-xl xxl:text-base xl:text-[13px]">Euro Avsallar Residence</span>
-              <div class="relative">
-                <span @click="openWatchTime = !openWatchTime" class="w-fit cursor-pointer flex items-center gap-2 xxl:gap-1.5 xl:gap-1">
-                  <img src="../../assets/svg/eye_icon_grey.svg" class="w-4 xxl:w-3.5 xl:w-3" alt="">
-                  <span class="text-[#8A8996] leading-none text-lg xxl:text-[15px] xl:text-[13px]"><span class="text-[#6435A5]"> 220 </span> {{ selectTime }}</span>
-                  <img src="../../assets/svg/arrow_down_black.svg" alt="">
-                </span>
-                <div v-if="openWatchTime" class="absolute top-[120%] text-lg xxl:text-[15px] xl:text-[13px] rounded-[3px] z-40 border bg-white border-solid border-[#E5DFEE] flex flex-col">
-                  <span
-                    v-for="(time, idx) in watchTime" :key="idx"
-                    @click="changeSelectTime(time)"
+                    @click="changeSelectTime(time, object)"
                     class="hover__select cursor-pointer pl-5 xxl:pl-4 xl:pl-3 pr-20 xxl:pr-16 xl:pr-12 py-3 xxl:py-2.5 xl:py-2 leading-none"
                   >
                     {{ time.time }}
@@ -122,11 +70,14 @@ import { Link } from '@inertiajs/inertia-vue3'
           <div class="hover__select h-7 xxl:h-6 xl:h-5 w-7 xxl:w-6 xl:w-5  rounded-[3px] flex items-center justify-center cursor-pointer">
             <img src="../../assets/svg/arrow_right_grey.svg" class="rotate-180 w-5 xxl:w-4 xl:w-3" alt="">
           </div>
-          <div class="hover__select h-7 xxl:h-6 xl:h-5 w-7 xxl:w-6 xl:w-5 rounded-[3px] flex items-center justify-center cursor-pointer">1</div>
-          <div class="hover__select h-7 xxl:h-6 xl:h-5 w-7 xxl:w-6 xl:w-5  rounded-[3px] flex items-center justify-center cursor-pointer">2</div>
-          <div class="hover__select h-7 xxl:h-6 xl:h-5 w-7 xxl:w-6 xl:w-5  rounded-[3px] flex items-center justify-center cursor-pointer">3</div>
-          <div class="hover__select h-7 xxl:h-6 xl:h-5 w-7 xxl:w-6 xl:w-5  rounded-[3px] flex items-center justify-center cursor-pointer">...</div>
-          <div class="hover__select h-7 xxl:h-6 xl:h-5 w-7 xxl:w-6 xl:w-5  rounded-[3px] flex items-center justify-center cursor-pointer">24</div>
+          <div
+            @click="tab.active = !tab.active"
+            v-for="tab in tabsPagination"
+            :class="{'select__pagination': tab.active}"
+            class="hover__select h-7 xxl:h-6 xl:h-5 w-7 xxl:w-6 xl:w-5  rounded-[3px] flex items-center justify-center cursor-pointer"
+          >
+            {{ tab.number }}
+          </div>
           <div class="hover__select h-7 xxl:h-6 xl:h-5 w-7 xxl:w-6 xl:w-5  rounded-[3px] flex items-center justify-center cursor-pointer">
             <img src="../../assets/svg/arrow_right_grey.svg" class="w-5 xxl:w-4 xl:w-3" alt="">
           </div>
@@ -142,8 +93,18 @@ import AppHeader from '../Layouts/AppHeader.vue'
 import AppFooter from "../Layouts/AppFooter.vue"
 
 export default {
+  props: {
+
+  },
   data() {
     return {
+      objects: [
+        {
+          name: '',
+          active: false
+        },
+        { name: '', active: false },
+      ],
       selectTime: 'за день',
       openWatchTime: false,
       watchTime: [
@@ -152,13 +113,20 @@ export default {
         { time: 'за неделю', value: 3 },
         { time: 'за месяц', value: 4 },
         { time: 'за год', value: 5 },
+      ],
+      tabsPagination: [
+        { number: '1', active: false },
+        { number: '2', active: false },
+        { number: '3', active: false },
+        { number: '...', active: false },
+        { number: '24', active: false },
       ]
     }
   },
   methods: {
-    changeSelectTime(time) {
+    changeSelectTime(time, object) {
       this.selectTime = time.time
-      this.openWatchTime = false
+      object.active = false
     }
   },
   components: {
