256cd459ccfd43762986a3d74d718200a675d1a7
Ivan Vaskovich <vaskov.vany1@gmail.com>
2022-12-18 11:34:02 +0100

    news

--- resources/js/Pages/AppAddNews.vue
+++ resources/js/Pages/AppAddNews.vue
@@ -19,11 +19,11 @@ import { Link } from '@inertiajs/inertia-vue3'
                 </div>
                 <div v-if="openSelectJK" class="absolute w-full z-40 bg-white flex flex-col top-full left-0 w-full border border-solid border-[#E5DFEE] rounded-b-[6px] text-lg xxl:text-[15px] xl:text-[13px]">
               <span
-                v-for="(JK, idx) in JKs" :key="idx"
+                v-for="(JK, idx) in houses" :key="idx"
                 @click="changeSelectJK(JK)"
                 class="hover__select cursor-pointer px-5 xxl:px-4 xl:px-3 py-3 xxl:py-2.5 xl:py-2 leading-none"
               >
-                {{ JK.JK }}
+                {{ JK.title }}
               </span>
                 </div>
               </div>
@@ -40,9 +40,9 @@ import { Link } from '@inertiajs/inertia-vue3'
                 theme="snow"
               />
             </div>
-            <button @click="addNews" class="bg-[#E84680] rounded-[5px] w-full py-5 xxl:py-4 xl:py-3">
-              <span class="text-white text-center font-semibold text-lg xxl:text-[15px] xl:text-[13px] leading-none">Добавить</span>
-            </button>
+            <Link href="/profile/news" @click="addNews" class="bg-[#E84680] rounded-[5px] w-full py-5 xxl:py-4 xl:py-3">
+              <div class="text-white text-center font-semibold text-lg xxl:text-[15px] xl:text-[13px] leading-none">Добавить</div>
+            </Link>
           </div>
         </div>
       </div>
@@ -59,18 +59,15 @@ import { QuillEditor } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css';
 
 export default {
+  props: {
+    houses: []
+  },
   data() {
     return {
       selectJK: 'Нажмите для выбора ЖК',
       openSelectJK: false,
-      JKs: [
-        { JK: 'Euro Avsallar Residence 1', value: 1 },
-        { JK: 'Euro Avsallar Residence 2', value: 2 },
-        { JK: 'Euro Avsallar Residence 3', value: 3 },
-        { JK: 'Euro Avsallar Residence 4', value: 4 },
-        { JK: 'Euro Avsallar Residence 5', value: 5 },
-      ],
       dataNews: {
+        house_id: null,
         title: null,
         description: null
       }
@@ -78,14 +75,16 @@ export default {
   },
   methods: {
     changeSelectJK(JK) {
-      this.selectJK = JK.JK
+      this.selectJK = JK.title
+      this.dataNews.house_id = JK.id
       this.openSelectJK = false
     },
     addNews() {
       axios.post('/api/news/add', {
-        house_id: null,
+        house_id: this.dataNews.house_id,
         title: this.dataNews.title,
-        description: this.dataNews.description
+        description: this.dataNews.description,
+        token: this.globalToken
       })
     },
   },
--- resources/js/Pages/AppNews.vue
+++ resources/js/Pages/AppNews.vue
@@ -1,13 +1,17 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3'
+</script>
+
 <template>
   <app-header />
   <main>
     <div class="_container">
       <div class="flex items-center justify-between mt-14 xxl:mt-10 xl:mt-8">
         <h2 class="text-[22px] xxl:text-lg xl:text-[15px] font-semibold">Новости</h2>
-        <button class="flex justify-center items-center gap-2 xl:gap-1.5 border border-solid rounded-[5px] border-[#6435A5] px-4 xxl:px-3 xl:px-2.5 py-2.5 xxl:py-2 xl:py-1.5">
+        <Link href="/profile/news/create" class="flex justify-center items-center gap-2 xl:gap-1.5 border border-solid rounded-[5px] border-[#6435A5] px-4 xxl:px-3 xl:px-2.5 py-2.5 xxl:py-2 xl:py-1.5">
           <img src="../../assets/svg/plus_icon_purple.svg" class="w-4 xxl:w-3.5 xl:w-3" alt="">
           <span class="text-[#6435A5] leading-none text-base xxl:text-sm xl:text-xs font-medium">Добавить новость</span>
-        </button>
+        </Link>
       </div>
       <div class="flex flex-col gap-5 xxl:gap-4 xl:gap-3 my-7 xxl:my-5 xl:my-4">
         <div class="grid__news text-[#8A8996] text-base xxl:text-sm xl:text-xs">
@@ -15,162 +19,40 @@
           <span class="p-5 xxl:p-4 xl:p-3">Заголовок</span>
           <span class="p-5 xxl:p-4 xl:p-3">Дата публикации</span>
         </div>
-        <div class="news__line flex justify-between items-center rounded-[12px]">
+        <div class="news__line flex justify-between items-center rounded-[12px]" v-for="item in news">
           <div class="grid__news text-base xxl:text-sm xl:text-xs">
-            <span class="p-5 xxl:p-4 xl:p-3">Euro Avsallar Residence</span>
-            <span class="p-5 xxl:p-4 xl:p-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</span>
+            <span class="p-5 xxl:p-4 xl:p-3">{{ item.house.title }}</span>
+            <span class="p-5 xxl:p-4 xl:p-3">{{ item.title }}</span>
             <span class="p-5 xxl:p-4 xl:p-3">24.11.22</span>
           </div>
           <div class="flex gap-4 xxl:gap-3 xl:gap-2.5 p-5 xxl:p-4 xl:p-3">
-            <button>
-              <img src="../../assets/svg/eye_icon_grey.svg" class="w-6 xxl:w-5 xl:w-4" alt="">
+            <button @click="changeVisible(item)">
+              <img v-if="item.visible" src="../../assets/svg/eye_icon_grey.svg" class="w-6 xxl:w-5 xl:w-4" alt="">
+              <img v-if="!item.visible" src="../../assets/svg/eye_close.svg" class="w-6 xxl:w-5 xl:w-4" alt="">
             </button>
             <button>
               <img src="../../assets/svg/pen_icon_grey.svg" class="w-6 xxl:w-5 xl:w-4" alt="">
             </button>
-            <button>
+            <button @click="delNews(item)">
               <img src="../../assets/svg/bucket_icon_red.svg" class="w-6 xxl:w-5 xl:w-4" alt="">
             </button>
           </div>
         </div>
-        <div class="news__line flex justify-between items-center rounded-[12px]">
-          <div class="grid__news text-base xxl:text-sm xl:text-xs">
-            <span class="p-5 xxl:p-4 xl:p-3">Euro Avsallar Residence</span>
-            <span class="p-5 xxl:p-4 xl:p-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</span>
-            <span class="p-5 xxl:p-4 xl:p-3">24.11.22</span>
-          </div>
-          <div class="flex gap-4 xxl:gap-3 xl:gap-2.5 p-5 xxl:p-4 xl:p-3">
-            <button>
-              <img src="../../assets/svg/eye_icon_grey.svg" class="w-6 xxl:w-5 xl:w-4" alt="">
-            </button>
-            <button>
-              <img src="../../assets/svg/pen_icon_grey.svg" class="w-6 xxl:w-5 xl:w-4" alt="">
-            </button>
-            <button>
-              <img src="../../assets/svg/bucket_icon_red.svg" class="w-6 xxl:w-5 xl:w-4" alt="">
-            </button>
-          </div>
-        </div>
-        <div class="news__line flex justify-between items-center rounded-[12px]">
-          <div class="grid__news text-base xxl:text-sm xl:text-xs">
-            <span class="p-5 xxl:p-4 xl:p-3">Euro Avsallar Residence</span>
-            <span class="p-5 xxl:p-4 xl:p-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</span>
-            <span class="p-5 xxl:p-4 xl:p-3">24.11.22</span>
-          </div>
-          <div class="flex gap-4 xxl:gap-3 xl:gap-2.5 p-5 xxl:p-4 xl:p-3">
-            <button>
-              <img src="../../assets/svg/eye_icon_grey.svg" class="w-6 xxl:w-5 xl:w-4" alt="">
-            </button>
-            <button>
-              <img src="../../assets/svg/pen_icon_grey.svg" class="w-6 xxl:w-5 xl:w-4" alt="">
-            </button>
-            <button>
-              <img src="../../assets/svg/bucket_icon_red.svg" class="w-6 xxl:w-5 xl:w-4" alt="">
-            </button>
-          </div>
-        </div>
-        <div class="news__line flex justify-between items-center rounded-[12px]">
-          <div class="grid__news text-base xxl:text-sm xl:text-xs">
-            <span class="p-5 xxl:p-4 xl:p-3">Euro Avsallar Residence</span>
-            <span class="p-5 xxl:p-4 xl:p-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</span>
-            <span class="p-5 xxl:p-4 xl:p-3">24.11.22</span>
-          </div>
-          <div class="flex gap-4 xxl:gap-3 xl:gap-2.5 p-5 xxl:p-4 xl:p-3">
-            <button>
-              <img src="../../assets/svg/eye_icon_grey.svg" class="w-6 xxl:w-5 xl:w-4" alt="">
-            </button>
-            <button>
-              <img src="../../assets/svg/pen_icon_grey.svg" class="w-6 xxl:w-5 xl:w-4" alt="">
-            </button>
-            <button>
-              <img src="../../assets/svg/bucket_icon_red.svg" class="w-6 xxl:w-5 xl:w-4" alt="">
-            </button>
-          </div>
-        </div>
-        <div class="news__line flex justify-between items-center rounded-[12px]">
-          <div class="grid__news text-base xxl:text-sm xl:text-xs">
-            <span class="p-5 xxl:p-4 xl:p-3">Euro Avsallar Residence</span>
-            <span class="p-5 xxl:p-4 xl:p-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</span>
-            <span class="p-5 xxl:p-4 xl:p-3">24.11.22</span>
-          </div>
-          <div class="flex gap-4 xxl:gap-3 xl:gap-2.5 p-5 xxl:p-4 xl:p-3">
-            <button>
-              <img src="../../assets/svg/eye_icon_grey.svg" class="w-6 xxl:w-5 xl:w-4" alt="">
-            </button>
-            <button>
-              <img src="../../assets/svg/pen_icon_grey.svg" class="w-6 xxl:w-5 xl:w-4" alt="">
-            </button>
-            <button>
-              <img src="../../assets/svg/bucket_icon_red.svg" class="w-6 xxl:w-5 xl:w-4" alt="">
-            </button>
-          </div>
-        </div>
-        <div class="news__line flex justify-between items-center rounded-[12px]">
-          <div class="grid__news text-base xxl:text-sm xl:text-xs">
-            <span class="p-5 xxl:p-4 xl:p-3">Euro Avsallar Residence</span>
-            <span class="p-5 xxl:p-4 xl:p-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</span>
-            <span class="p-5 xxl:p-4 xl:p-3">24.11.22</span>
-          </div>
-          <div class="flex gap-4 xxl:gap-3 xl:gap-2.5 p-5 xxl:p-4 xl:p-3">
-            <button>
-              <img src="../../assets/svg/eye_icon_grey.svg" class="w-6 xxl:w-5 xl:w-4" alt="">
-            </button>
-            <button>
-              <img src="../../assets/svg/pen_icon_grey.svg" class="w-6 xxl:w-5 xl:w-4" alt="">
-            </button>
-            <button>
-              <img src="../../assets/svg/bucket_icon_red.svg" class="w-6 xxl:w-5 xl:w-4" alt="">
-            </button>
-          </div>
-        </div>
-        <div class="news__line flex justify-between items-center rounded-[12px]">
-          <div class="grid__news text-base xxl:text-sm xl:text-xs">
-            <span class="p-5 xxl:p-4 xl:p-3">Euro Avsallar Residence</span>
-            <span class="p-5 xxl:p-4 xl:p-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</span>
-            <span class="p-5 xxl:p-4 xl:p-3">24.11.22</span>
-          </div>
-          <div class="flex gap-4 xxl:gap-3 xl:gap-2.5 p-5 xxl:p-4 xl:p-3">
-            <button>
-              <img src="../../assets/svg/eye_icon_grey.svg" class="w-6 xxl:w-5 xl:w-4" alt="">
-            </button>
-            <button>
-              <img src="../../assets/svg/pen_icon_grey.svg" class="w-6 xxl:w-5 xl:w-4" alt="">
-            </button>
-            <button>
-              <img src="../../assets/svg/bucket_icon_red.svg" class="w-6 xxl:w-5 xl:w-4" alt="">
-            </button>
-          </div>
-        </div>
-        <div class="news__line flex justify-between items-center rounded-[12px]">
-          <div class="grid__news text-base xxl:text-sm xl:text-xs">
-            <span class="p-5 xxl:p-4 xl:p-3">Euro Avsallar Residence</span>
-            <span class="p-5 xxl:p-4 xl:p-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</span>
-            <span class="p-5 xxl:p-4 xl:p-3">24.11.22</span>
-          </div>
-          <div class="flex gap-4 xxl:gap-3 xl:gap-2.5 p-5 xxl:p-4 xl:p-3">
-            <button>
-              <img src="../../assets/svg/eye_icon_grey.svg" class="w-6 xxl:w-5 xl:w-4" alt="">
-            </button>
-            <button>
-              <img src="../../assets/svg/pen_icon_grey.svg" class="w-6 xxl:w-5 xl:w-4" alt="">
-            </button>
-            <button>
-              <img src="../../assets/svg/bucket_icon_red.svg" class="w-6 xxl:w-5 xl:w-4" alt="">
-            </button>
-          </div>
-        </div>
+        <div class="news__line flex justify-between items-center rounded-[12px]" v-if="news.length === 0">
+          <h1 class="text-center">Новостей нет</h1>
       </div>
-      <div class="w-full flex justify-center gap-7 xxl:gap-5 xl:gap-4 my-14 xxl:my-10 xl:my-8 text-[22px] xxl:text-lg xl:text-[15px]">
-        <div class="hover__select h-7 xxl:h-6 xl:h-5 w-7 xxl:w-6 xl:w-5  rounded-[3px] flex items-center justify-center cursor-pointer">
-          <img src="../../assets/svg/arrow_right_grey.svg" class="rotate-180 w-8 xxl:w-6 xl:w-5" alt="">
-        </div>
-        <div class="hover__select h-7 xxl:h-6 xl:h-5 w-7 xxl:w-6 xl:w-5 rounded-[3px] flex items-center justify-center cursor-pointer">1</div>
-        <div class="hover__select h-7 xxl:h-6 xl:h-5 w-7 xxl:w-6 xl:w-5 rounded-[3px] flex items-center justify-center cursor-pointer">2</div>
-        <div class="hover__select h-7 xxl:h-6 xl:h-5 w-7 xxl:w-6 xl:w-5 rounded-[3px] flex items-center justify-center cursor-pointer">3</div>
-        <div class="hover__select h-7 xxl:h-6 xl:h-5 w-7 xxl:w-6 xl:w-5 rounded-[3px] flex items-center justify-center cursor-pointer">...</div>
-        <div class="hover__select h-7 xxl:h-6 xl:h-5 w-7 xxl:w-6 xl:w-5 rounded-[3px] flex items-center justify-center cursor-pointer">24</div>
-        <div class="hover__select h-7 xxl:h-6 xl:h-5 w-7 xxl:w-6 xl:w-5 rounded-[3px] flex items-center justify-center cursor-pointer">
-          <img src="../../assets/svg/arrow_right_grey.svg" class="w-8 xxl:w-6 xl:w-5" alt="">
+        <div class="w-full flex justify-center gap-7 xxl:gap-5 xl:gap-4 my-14 xxl:my-10 xl:my-8 text-[22px] xxl:text-lg xl:text-[15px]">
+          <div class="hover__select h-7 xxl:h-6 xl:h-5 w-7 xxl:w-6 xl:w-5  rounded-[3px] flex items-center justify-center cursor-pointer">
+            <img src="../../assets/svg/arrow_right_grey.svg" class="rotate-180 w-8 xxl:w-6 xl:w-5" alt="">
+          </div>
+          <div class="hover__select h-7 xxl:h-6 xl:h-5 w-7 xxl:w-6 xl:w-5 rounded-[3px] flex items-center justify-center cursor-pointer">1</div>
+          <div class="hover__select h-7 xxl:h-6 xl:h-5 w-7 xxl:w-6 xl:w-5 rounded-[3px] flex items-center justify-center cursor-pointer">2</div>
+          <div class="hover__select h-7 xxl:h-6 xl:h-5 w-7 xxl:w-6 xl:w-5 rounded-[3px] flex items-center justify-center cursor-pointer">3</div>
+          <div class="hover__select h-7 xxl:h-6 xl:h-5 w-7 xxl:w-6 xl:w-5 rounded-[3px] flex items-center justify-center cursor-pointer">...</div>
+          <div class="hover__select h-7 xxl:h-6 xl:h-5 w-7 xxl:w-6 xl:w-5 rounded-[3px] flex items-center justify-center cursor-pointer">24</div>
+          <div class="hover__select h-7 xxl:h-6 xl:h-5 w-7 xxl:w-6 xl:w-5 rounded-[3px] flex items-center justify-center cursor-pointer">
+            <img src="../../assets/svg/arrow_right_grey.svg" class="w-8 xxl:w-6 xl:w-5" alt="">
+          </div>
         </div>
       </div>
     </div>
@@ -183,15 +65,31 @@ import AppFooter from "@/Layouts/AppFooter.vue"
 import AppHeader from "@/Layouts/AppHeader.vue"
 
 export default {
+  props: {
+    news: []
+  },
   data() {
     return {
-
+    }
+  },
+  methods: {
+    delNews(news) {
+      this.news.forEach((item, idx) => {
+        if (item.id === news.id) {
+          this.news.splice(idx, 1)
+        }
+      })
+      axios.post('/api/news/delete', { new_id: news.id, token: this.globalToken })
+    },
+    changeVisible(item) {
+      item.visible = !item.visible
+      axios.post('/api/news/visible', { new_id: item.id, visible: item.visible, token: this.globalToken })
     }
   },
   components: {
     AppHeader,
     AppFooter,
-  }
+  },
 }
 </script>
 
