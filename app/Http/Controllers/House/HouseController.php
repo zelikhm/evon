94b84369e895772fc7d7ae2cafcde2a123bd6bf8
Nikita <boss.Zelikov99@gmail.com>
2023-01-19 22:29:27 +0300

    Update

--- app/Http/Controllers/House/HouseController.php
+++ app/Http/Controllers/House/HouseController.php
@@ -40,8 +40,6 @@ class HouseController extends Controller
 
     $houses = $this->getAllHouse();
 
-    dd($this->getNewsForPage());
-
     return Inertia::render('AppListImmovables', [
       'houses' => $houses,
       'dops' => TypesModel::all(),
