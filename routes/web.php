3d88cbd9ad8a6f633944e89a51ed84fc8dec866b
Nikita <boss.Zelikov99@gmail.com>
2023-01-30 23:45:02 +0300

    Update

--- routes/web.php
+++ routes/web.php
@@ -34,14 +34,15 @@ Route::prefix('profile')->middleware(['auth', 'session'])->group(function () {
     Route::get('/news/edit/{id}', ['App\Http\Controllers\House\NewsController', 'editNews']);
   });
 
+  Route::get('/compilation', ['App\Http\Controllers\User\CompilationController', 'index']);
+
   Route::get('/favorites', ['App\Http\Controllers\User\FavoriteController', 'index']);
 //
   Route::get('/chat', ['App\Http\Controllers\User\ChatController', 'index']);
 
 });
 
-Route::prefix('/profile/compilation')->group(function () {
-  Route::get('/', ['App\Http\Controllers\User\CompilationController', 'index']);
+Route::prefix('/compilation')->group(function () {
   Route::get('/{id}', ['App\Http\Controllers\User\CompilationController', 'show']);
   Route::get('/{id}/{house}', ['App\Http\Controllers\User\CompilationController', 'house']);
 });
