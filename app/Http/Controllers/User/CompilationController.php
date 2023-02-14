4a7331c150ac51372435d5d2c7b41c9d902cf16e
Zelikhm <boss.Zelikov99@gmail.com>
2023-02-14 14:55:30 +0300

    Update

--- app/Http/Controllers/User/CompilationController.php
+++ app/Http/Controllers/User/CompilationController.php
@@ -139,6 +139,8 @@ class CompilationController extends Controller
 
     $compilation = CompilationModel::where('id', $id - 10000)->with(['values', 'user', 'company'])->firstOrFail();
 
+    dd($house);
+
     return Inertia::render('AppLinkDescriptionObject', [
       'house' => $house,
       'compilation' => $compilation,
