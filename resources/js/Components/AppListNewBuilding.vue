5e466169b4d921ff9721665a284af557d4cd5411
Ivan Vaskovich <vaskov.vany1@gmail.com>
2023-02-18 08:46:57 +0100

    tasks

--- resources/js/Components/AppListNewBuilding.vue
+++ resources/js/Components/AppListNewBuilding.vue
@@ -514,9 +514,10 @@
           </div>
         </div>
         <div class="w-full flex justify-center mb-14 xxl:mb-10 xl:mb-8" @click="nextShow()"
-             v-if="count_house > count && !map">
+             v-if="count_house > count && !map"
+             >
           <button
-            class="more__button transition-all text-[#E84680] border border-solid border-[#E84680] text-base xxl:text-sm xl:text-xs lg:text-[15px] px-6 xxl:px-5 xl:px-4 py-2.5 xxl:py-2.5 xl:py-1.5 rounded-[3px]">
+            class="more__button mt-10 transition-all text-[#E84680] border border-solid border-[#E84680] text-base xxl:text-sm xl:text-xs lg:text-[15px] px-6 xxl:px-5 xl:px-4 py-2.5 xxl:py-2.5 xl:py-1.5 rounded-[3px]">
             Показать еще
           </button>
         </div>
