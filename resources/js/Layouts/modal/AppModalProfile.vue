e0bd1c6ffcc8578d20b0acf3a561e4aa89471e60
Nikita Zelikov <boss.Zelikov99@gmail.com>
2023-03-10 14:37:54 +0300

    Update

--- resources/js/Layouts/modal/AppModalProfile.vue
+++ resources/js/Layouts/modal/AppModalProfile.vue
@@ -11,7 +11,7 @@
       <div v-for="item in tarifs" class="flex justify-between items-center border border-solid border-[#E5DFEE] rounded-[5px] p-5 xxl:p-4 xl:p-3">
         <label class="payd__checkbox" v-on:click="setDate(item)">
           <input type="radio" value="1" id="pay_1" name="payd" >
-          <span class="text-[18px] xxl:text-[15px] xl:text-[13px]" for="pay_1">{{ item.days }} {{ item.title }}</span>
+          <span class="text-[18px] xxl:text-[15px] xl:text-[13px]" for="pay_1">{{ item.title }}</span>
         </label>
         <span class="text-[18px] xxl:text-[15px] xl:text-[13px]">{{ item.price / 100 }} TL</span>
       </div>
