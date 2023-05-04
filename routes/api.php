7a1fcb46739ff19f4126c8ce95e82a180e23ca27
Nikita Zelikov <boss.Zelikov99@gmail.com>
2023-05-04 20:21:37 +0300

    register

--- routes/api.php
+++ routes/api.php
@@ -100,7 +100,7 @@ Route::prefix('user')->group(function () {
   Route::post('/lang', ['App\Http\Controllers\User\IndexController', 'lang']);
   Route::post('/setLink', ['App\Http\Controllers\User\IndexController', 'setLink']);
   //
-  Route::post('register', ['App\Http\Controllers\User\IndexController', 'sendRegister']);
+  Route::post('/register', ['App\Http\Controllers\User\IndexController', 'sendRegister']);
 });
 
 Route::prefix('chat')->group(function () {
