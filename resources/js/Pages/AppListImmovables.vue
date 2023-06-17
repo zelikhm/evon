a733288860eaa536f6b25c6e7e239ccb8c46af0d
logan <test.vue@bk.ru>
2023-06-17 19:56:49 +0300

    UPDATE

--- resources/js/Pages/AppListImmovables.vue
+++ resources/js/Pages/AppListImmovables.vue
@@ -38,13 +38,13 @@ import { Link } from '@inertiajs/inertia-vue3'
   <main>
     <div class="_container">
       <div class="hints ">
-         <p class="text-[#E84680]   transition-all   text-xl xxl:text-base xl:text-sm md:text-[17px]">Бесплатная версия. Доступно:</p>
+         <p class="text-[#E84680]   transition-all   text-xl xxl:text-base xl:text-sm md:text-[17px]">{{language.free_version[0]}}:</p>
          <div class="progress">
            <div class="line" :style="{width:`${getWidth(count_houses)}%`}"></div>
-           <h4 class="text-[#6435A5] text-lg xxl:text-[15px] xl:text-[13px] md:text-[17px]">30 объектов из {{ count_houses }}</h4>
+           <h4 class="text-[#6435A5] text-lg xxl:text-[15px] xl:text-[13px] md:text-[17px]">30 {{language.free_version[1]}}{{ count_houses }}</h4>
          </div>
          <Link href="/profile"  >
-           <button class="cursor-pointer uppercase   text-[#E84680] text-sm xxl:text-xs xl:text-[10px] md:text-[12px] leading-none font-semibold rounded-[3px] px-8 xxl:px-8 xl:px-8 py-2 xxl:py-1.5 xl:py-1 cursor-default" >Купить</button>
+           <button class="cursor-pointer uppercase   text-[#E84680] text-sm xxl:text-xs xl:text-[10px] md:text-[12px] leading-none font-semibold rounded-[3px] px-8 xxl:px-8 xl:px-8 py-2 xxl:py-1.5 xl:py-1 cursor-default" >{{language.free_version[2]}}</button>
          </Link>
          
       </div>
