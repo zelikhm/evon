32671eff1023d9b718b3240570b01c138daed9f2
Nikita Zelikov <boss.Zelikov99@gmail.com>
2023-08-24 12:37:17 +0300

    Update

--- app/Http/Traits/MainInfo.php
+++ app/Http/Traits/MainInfo.php
@@ -373,11 +373,11 @@ trait MainInfo
 
       $house->popular = HouseViewsModel::where('house_id', $house->id)->count() > 30;
 
-      $house->maxPrice = round($house->flats->max('price'));
-      $house->minPrice = round($house->flats->min('price'));
+      $house->maxPrice = (int)round($house->flats->max('price'));
+      $house->minPrice = (int)round($house->flats->min('price'));
 
-      $house->maxSquare = round($house->flats->max('square'));
-      $house->minSquare = round($house->flats->min('square'));
+      $house->maxSquare = (int)round($house->flats->max('square'));
+      $house->minSquare = (int)round($house->flats->min('square'));
 
       $house->builder = $house->user->first_name;
 
