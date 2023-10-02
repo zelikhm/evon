b2effa94bc37a897f3cd70660c4328d73950ba88
Nikita Zelikov <boss.Zelikov99@gmail.com>
2023-10-02 13:27:58 +0300

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    server: {
-      https: false,
-      hmr : {
-        host: 'http://159.253.23.123:7777/evon.test',
-      },
-    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
