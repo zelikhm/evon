54fd7e12bd9f511bea8579b8304aa671aecf1575
logan <test.vue@bk.ru>
2023-10-23 16:38:48 +0300

    UPDATE

--- resources/js/Layouts/modal/AppSubmitSelection.vue
+++ resources/js/Layouts/modal/AppSubmitSelection.vue
@@ -2,7 +2,7 @@
   <div class="fixed z-[100] w-full h-full flex items-center justify-center">
     <div class="relative bg-white rounded-[12px] p-10 xxl:p-9 xl:p-8 lg:p-5 z-50 w-[30%] lg:w-[48%] md:w-[60%] sm:w-[90%] h-fit">
       <div class="flex justify-between items-center mb-5 xxl:mb-4 xl:mb-3">
-        <h2 class="text-[22px] xxl:text-[18px] xl:text-[16px] font-semibold leading-none">{{ language.podbor_1[1] }}</h2>
+        <h2 class="text-[22px] xxl:text-[18px] xl:text-[16px] font-semibold leading-none">{{ language?.podbor_1[1]??title }}</h2>
         <button @click="$emit('closeSubmitSelection')" class="hover__close right-0 z-50">
           <svg class="transition-all cursor-pointer w-6 xxl:w-5 xl:w-4" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g clip-path="url(#clip0_266_4120)">
@@ -16,11 +16,11 @@
           </svg>
         </button>
       </div>
-      <span class="text-[#8A8996] text-[14px] xxl:text-[12px] xl:text-[10px] leading-none">{{ language.podbor_1[0] }}</span>
+      <span class="text-[#8A8996] text-[14px] xxl:text-[12px] xl:text-[10px] leading-none">{{ language?.podbor_1[0]??"" }}</span>
       <div class="relative mt-10 xxl:mt-8 xl:mt-6">
         <input :value="valueLink" ref="text" disabled class="copy-link w-full text-[#1E1D2D] text-[16px] focus:ring-[#6435A5] focus:border-[#6435A5] xxl:text-[14px] xl:text-[12px] pl-5 xxl:pl-4 xl:pl-3 py-3 xxl:py-2.5 xl:py-2 leading-none rounded-[5px] border border-solid border-[#E5DFEE] pr-12 xxl:pr-10 xl:pr-8" type="text">
         <!-- <span  v-if="user.subscription_info.free == 1"  class="copy-link-text">{{language.ob[50]}}</span>       -->
-        <span v-if="copy_link_hint" class="copy-link-text">Ссылка скопирована</span>   
+        <span v-if="copy_link_hint" class="copy-link-text">Ссылка скопирована</span>
         <img @click="copy" class="absolute cursor-pointer top-1/2 -translate-y-1/2 w-6 xxl:w-5 xl:w-4 right-5 xxl:right-4 xl:right-3" src="../../../assets/svg/copy_icon_purple.svg" alt="">
       </div>
     </div>
@@ -33,57 +33,64 @@ export default {
   props: {
     link: String,
     user: Object,
-    language: {}
+    language: {},
+    title:{
+       type:String,
+       default:""
+    },
+    type:{
+      type:Number,
+      default:0
+    }
   },
   emits: ['closeSubmitSelection'],
   data() {
     return {
-      valueLink: 'http://102895812h9051h2081102j9',
+      valueLink: this.type==0?'http://102895812h9051h2081102j9':this.link,
       copy_link_hint:false
     }
   },
   methods: {
-    copy() { 
+    copy() {
       try {
-        navigator.clipboard.writeText(this.valueLink)  
+        navigator.clipboard.writeText(this.valueLink)
         this.copy_link_hint=true
         setTimeout(() => {
           this.copy_link_hint=false
         }, 3000);
       } catch (error) {
-        var textToCopy = this.valueLink;   
+        var textToCopy = this.valueLink;
         var input = document.createElement("textarea");
         input.value = textToCopy;
         document.body.appendChild(input);
         input.select();
-        input.setSelectionRange(0, 99999); 
+        input.setSelectionRange(0, 99999);
         document.execCommand("copy");
-        document.body.removeChild(input); 
+        document.body.removeChild(input);
         this.copy_link_hint=true
         setTimeout(() => {
           this.copy_link_hint=false
         }, 3000);
-      } 
+      }
     }
   },
   created() {
-    console.log(this.link)
-    this.valueLink = this.link 
+    this.valueLink = this.link
   }
 }
 </script>
 
 <style scoped>
-.copy-link-text {  
+.copy-link-text {
   position: absolute;
     top: 40px;
     right: 99px;
     box-shadow: 0px 0px 0px 1px;
     background: white;
     color: rgb(101 54 165 );
-    border-radius: 10px; 
+    border-radius: 10px;
     font-size: 12px;
-    padding: 2px 22px; 
+    padding: 2px 22px;
     transition: .7s;
     border: 7px;
     pointer-events: none;
