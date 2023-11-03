7200c4891f123dac237bd65a2e9b9c36fafa0b13
logan <test.vue@bk.ru>
2023-11-03 12:18:51 +0300

    UPDATE

--- resources/js/Layouts/modal/AppMoadalAddClient.vue
+++ resources/js/Layouts/modal/AppMoadalAddClient.vue
@@ -6,10 +6,10 @@
 
         <div class="flex justify-between items-center mb-5 xxl:mb-4 xl:mb-3">
           <h2 class="text-[22px] xxl:text-[18px] xl:text-[16px] font-semibold leading-none">{{ language.send_client[0]
-          }}</h2>
+            }}</h2>
           <button class="hover__close right-0 z-50">
             <svg class="transition-all cursor-pointer w-6 xxl:w-5 xl:w-4" width="24" @click="closeAddClient" height="24"
-              viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                 viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <g clip-path="url(#clip0_266_4120)">
                 <path
                   d="M11.9997 10.586L16.9497 5.63599L18.3637 7.04999L13.4137 12L18.3637 16.95L16.9497 18.364L11.9997 13.414L7.04974 18.364L5.63574 16.95L10.5857 12L5.63574 7.04999L7.04974 5.63599L11.9997 10.586Z"
@@ -27,12 +27,12 @@
         <div class="relative mt-10 xxl:mt-8 xl:mt-6">
 
           <input :value="getLink()" readonly ref="text" disabled
-            class="copy-link w-full text-[#1E1D2D] text-[16px] focus:ring-[#6435A5] focus:border-[#6435A5] xxl:text-[14px] xl:text-[12px] pl-5 xxl:pl-4 xl:pl-3 py-3 xxl:py-2.5 xl:py-2 leading-none rounded-[5px] border border-solid border-[#E5DFEE] pr-12 xxl:pr-10 xl:pr-8"
-            type="text">
-                   <span class="copy-link-text">{{ language.subscription[76] }}</span>
+                 class="copy-link w-full text-[#1E1D2D] text-[16px] focus:ring-[#6435A5] focus:border-[#6435A5] xxl:text-[14px] xl:text-[12px] pl-5 xxl:pl-4 xl:pl-3 py-3 xxl:py-2.5 xl:py-2 leading-none rounded-[5px] border border-solid border-[#E5DFEE] pr-12 xxl:pr-10 xl:pr-8"
+                 type="text">
+          <span v-if="copy_link_hint" class="copy-link-text">Ссылка скопирована</span>
           <img @click="copy"
-            class="absolute cursor-pointer top-1/2 -translate-y-1/2 w-6 xxl:w-5 xl:w-4 right-5 xxl:right-4 xl:right-3"
-            src="../../../assets/svg/copy_icon_purple.svg" alt="">
+               class="absolute cursor-pointer top-1/2 -translate-y-1/2 w-6 xxl:w-5 xl:w-4 right-5 xxl:right-4 xl:right-3"
+               src="../../../assets/svg/copy_icon_purple.svg" alt="">
         </div>
 
       </div>
@@ -52,6 +52,7 @@ export default {
     return {
       isVisible: false,
       selectLanguage: 0,
+      copy_link_hint:false
     }
   },
   mounted() {
@@ -60,16 +61,29 @@ export default {
   emits: ['close-addClient'],
   methods: {
     getLink() {
+
+      if (this.user.subscription_info.free_link == 1) {
+        axios.post('/api/user/setLink', {
+          'token': this.user.token,
+          'id': this.user.subscription_info.id,
+        })
+      }
+
       return window.location.origin + '/compilation/user/' + this.user.id + '/' + this.house.slug;
+
     },
     closeAddClient() {
       this.$emit('close-addClient')
     },
     copy() {
       try {
-        navigator.clipboard.writeText(this.getLink())
+        navigator.clipboard.writeText(this.valueLink)
+        this.copy_link_hint=true
+        setTimeout(() => {
+          this.copy_link_hint=false
+        }, 3000);
       } catch (error) {
-        var textToCopy = this.getLink();
+        var textToCopy = this.valueLink;
         var input = document.createElement("textarea");
         input.value = textToCopy;
         document.body.appendChild(input);
@@ -77,6 +91,10 @@ export default {
         input.setSelectionRange(0, 99999);
         document.execCommand("copy");
         document.body.removeChild(input);
+        this.copy_link_hint=true
+        setTimeout(() => {
+          this.copy_link_hint=false
+        }, 3000);
       }
     }
   }
